70adc97f06e0a9892b80cc70acab68b1b501b6a0
aditipandey25009-hue <aditipandey25009@gmail.com>
2026-02-03 20:49:43 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,9 @@
+marks= int(input("enter marks:"))
+if marks<40:
+    print("fail")
+elif 40<marks<=60:
+    print("average")
+elif 60<marks<=80:
+    print("above average")
+else:
+        print("pass")
---
+++ momentum.py
@@ -0,0 +1,4 @@
+m=float(input("enter m"))
+v= float(input("enter v"))
+mon = m*v
+print(mon)
