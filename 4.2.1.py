425d019c952626aeb49ea000720846c69595c933
aditipandey25009-hue <aditipandey25009@gmail.com>
2026-04-27 13:03:29 +0530

    Add files via upload

---
+++ 4.2.1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+file_name = input()
+df = pd.read_csv(file_name)
+df['Date'] = pd.to_datetime(df['Date'])
+df['Sales'] = df['Quantity'] * df['Price']
+df['Month'] = df['Date'].dt.to_period('M')
+monthly_sales = df.groupby('Month')['Sales'].sum()
+best_month = monthly_sales.idxmax()
+highest_sales = monthly_sales.max()
+print(f"Best month: {best_month}")
+print(f"Total sales: ${highest_sales:.2f}")
